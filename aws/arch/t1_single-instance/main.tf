b4c4d20a86d6fefcefb1c8ccb323543c1f7d0f40
alexandervcc <alex.charco.25@gmail.com>
2024-09-11 18:34:47 -0500

    Centralize Apache http server script

--- aws/arch/t1_single-instance/main.tf
+++ aws/arch/t1_single-instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_http_server" {
   ami           = "ami-09efc42336106d2f2" 
   instance_type = "t2.micro"
-  user_data = file("${path.module}/ec2-http-server.sh")
+  user_data = file("${path.module}/../ec2-http-server.sh")
   tags = {
     "base" : "simple-ec2-instance"
   }
