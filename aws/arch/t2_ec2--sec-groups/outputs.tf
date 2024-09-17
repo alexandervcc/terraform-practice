11749ca6152ca74a22f859d9084d1a8b3edcd223
alexandervcc <alex.charco.25@gmail.com>
2024-09-16 19:27:37 -0500

    T2: for-loop outputs

--- aws/arch/t2_ec2--sec-groups/outputs.tf
+++ aws/arch/t2_ec2--sec-groups/outputs.tf
@@ -1,3 +1,6 @@
+/*
+For a single instance, without count config.
+
 output "instance_public_ip" {
   description = "EC2 instance public IP."
   value       = aws_instance.ec2_http_server.public_ip
@@ -6,4 +9,27 @@ output "instance_public_ip" {
 output "intance_public_dns" {
   description = "EC2 instance public DNS"
   value       = aws_instance.ec2_http_server.public_dns
+}
+*/
+
+# List
+output "for-list-instances" {
+  value       = [for instance in aws_instance.ec2_http_server : instance.public_dns]
+}
+
+# Map 
+output "for-map-instaces" {
+  value = {
+    for instance in aws_instance.ec2_http_server : instance.id => instance.public_dns
+  }
+}
+
+output "for-map-advanced-instances" {
+  value = {
+    for index, instance in aws_instance.ec2_http_server : index => instance.public_dns
+  }
+}
+
+output "for-splat-operator-instances" {
+  value = aws_instance.ec2_http_server[*].public_dns
 }
