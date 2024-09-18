94d25214f036cfa1def648a9dd5fc6d61de02037
alexandervcc <alex.charco.25@gmail.com>
2024-09-18 17:58:15 -0500

    T2: Use validated azones on EC2 instances
    xd

--- aws/arch/t2_ec2--sec-groups/ec2-instance.tf
+++ aws/arch/t2_ec2--sec-groups/ec2-instance.tf
@@ -19,7 +19,14 @@ resource "aws_instance" "ec2_http_server" {
   # count = 2
 
   # Create an instance on differente Availability zones
-  for_each          = toset(data.aws_availability_zones.my-azones.names)
+  # for_each          = toset(data.aws_availability_zones.my-azones.names)
+
+  # Create an instance on differente Availability zones, which passed validations
+  for_each = toset(keys({
+    for az, details in data.aws_ec2_instance_type_offerings.my-instance-types :
+    az => details.instance_types if length(details.instance_types) != 0
+  }))
+
   availability_zone = each.key
 
   tags = {
